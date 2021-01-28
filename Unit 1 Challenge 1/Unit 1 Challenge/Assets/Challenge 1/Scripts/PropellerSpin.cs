ba60b59f879a6a51d0f629cd4eb7f3fe459f10e0
Darick Cochrane <boomrocks24@gmail.com>
2021-01-27 23:30:42 -0700

    Challenge 1 Complete

---
+++ Unit 1 Challenge 1/Unit 1 Challenge/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    public float turnSpeed = 10.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * turnSpeed);
+    }
+}
