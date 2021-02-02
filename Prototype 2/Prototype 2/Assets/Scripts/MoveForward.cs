9591f1de71acec2527fd34ef81b747aa8516220c
Darick Cochrane <boomrocks24@gmail.com>
2021-02-01 17:21:03 -0700

    Created Protoype 2 Animal Stampede

---
+++ Prototype 2/Prototype 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speedValue = 20.0f;     
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(-15,2,-8);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * speedValue);
+
+    }
+}
