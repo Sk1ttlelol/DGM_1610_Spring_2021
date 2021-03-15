bb1c4145950d42ca26a73c5dd4f873a21a4856ba
Darick Cochrane <boomrocks24@gmail.com>
2021-03-15 17:13:51 -0600

    Finished Prototype 3

--- Prototype 3/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,11 @@ public class PlayerController : MonoBehaviour
     public bool isOnGround = true;
     public bool isGameOver = false;
     private Animator playerAnim;
+    public ParticleSystem dirtParticles;
+    public ParticleSystem explosionParticles;
+    public AudioClip jumpSound;
+    public AudioClip crashSound;
+    private AudioSource playerAudio;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +23,7 @@ public class PlayerController : MonoBehaviour
         Physics.gravity *= gravityMod; 
 
         playerAnim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,6 +35,8 @@ public class PlayerController : MonoBehaviour
           isOnGround = false;
 
           playerAnim.SetTrigger("Jump_trig");
+          dirtParticles.Stop();
+          playerAudio.PlayOneShot(jumpSound, 1.0f);
       }
     }
 
@@ -40,6 +48,7 @@ public class PlayerController : MonoBehaviour
         {
             isOnGround = true;
             Debug.Log("Grounded");
+            dirtParticles.Play();
         }
         else if(collision.gameObject.CompareTag("Obstacle"))
         {
@@ -47,6 +56,9 @@ public class PlayerController : MonoBehaviour
             Debug.Log("Game Over oof");
             playerAnim.SetBool("Death_b",true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticles.Play();
+            dirtParticles.Stop();
+            playerAudio.PlayOneShot(crashSound, 1.0f);
         }
 
 
