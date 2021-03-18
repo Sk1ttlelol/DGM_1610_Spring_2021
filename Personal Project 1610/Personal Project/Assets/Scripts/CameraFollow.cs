3a3e70064f6113e7eb41f0e51d53cfa7206c9132
Darick Cochrane <boomrocks24@gmail.com>
2021-03-17 21:21:37 -0600

    Lab 3 Complete, code noted and cleaned

---
+++ Personal Project 1610/Personal Project/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    
+    public float speed;
+    public float hInput;
+    public float leftBound = 11;
+    //variable to grab player controller script
+    private PlayerController playerControllerScript;
+
+    public Vector3 cameraSpawnPos = new Vector3(10,17,16);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = cameraSpawnPos;
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * speed *hInput);
+
+        //if position of camera goes too far left, stop movement
+        if(transform.position.x > leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        }
+        //if isTakeDamage from playerControllerScript becomes true, move camera to respawn postiion to continue following player
+        if(playerControllerScript.isTakeDamage == true)
+        {
+            transform.position = cameraSpawnPos;
+        }
+    }
+}
---
+++ Personal Project 1610/Personal Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float hInput;
+    public float vInput;
+    public float jumpForce;
+    private Rigidbody playerRb;
+    public float gravityMod;
+    public float leftBound = 33;
+    public Vector3 playerSpawnPos = new Vector3(32,10,-5);
+
+    public bool isTakeDamage = false;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {   //Grabs the Rigidbody component from the player
+        playerRb = GetComponent<Rigidbody>();
+
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Grabs the axis for control
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.left * Time.deltaTime * speed *hInput);
+
+        //if player pushes space bar, player go boing boing
+      if(Input.GetKeyDown(KeyCode.Space))
+      {
+          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+      }
+      //if player goes too far to the left, stop movement
+      if(transform.position.x > leftBound)
+        {
+            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {   //if player touches respawn platform, isTakeDamage becomes false
+        if(other.gameObject.CompareTag("Respawn"))
+        {
+            isTakeDamage = false;
+        }
+        //if player touches ground, spikes, or enemies, isTakeDamage becomes true
+        if(other.gameObject.CompareTag("takeDamage"))
+        {
+            isTakeDamage = true; 
+        }
+        //if isTakeDamage becomes true, move player to respawn position
+        if(isTakeDamage == true)
+        {
+            transform.position = playerSpawnPos;
+
+        }
+
+    }
+}
