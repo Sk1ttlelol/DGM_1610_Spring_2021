1f4029b06d770d146a947bc2e49779528be42712
Darick Cochrane <boomrocks24@gmail.com>
2021-04-07 16:09:44 -0600

    Trying to get double jump and jump duration to work together, may have to only do double jump

--- Personal Project 1610/Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project 1610/Personal Project/Assets/Scripts/PlayerController.cs
@@ -8,35 +8,79 @@ public class PlayerController : MonoBehaviour
     public float hInput;
     public float vInput;
     public float jumpForce;
+
     private Rigidbody playerRb;
     public float gravityMod;
     public float leftBound = 33;
-    public Vector3 playerSpawnPos = new Vector3(32,10,-5);
+    public Vector3 playerSpawnPos = new Vector3(32,14,-5);
 
     public bool isTakeDamage = false;
+    public bool isOnGround = true;
 
+    private float jumpTimeCounter;
+    public float jumpTime;
+    private bool isJumping;
+    private int extraJumps;
+    public int extraJumpsValue;
+
+    private bool hasWings = false;
     
 
     // Start is called before the first frame update
     void Start()
     {   //Grabs the Rigidbody component from the player
         playerRb = GetComponent<Rigidbody>();
+        extraJumps = extraJumpsValue;
 
         Physics.gravity *= gravityMod;
     }
 
     // Update is called once per frame
     void Update()
-    {   //Grabs the axis for control
+    {   
+       //playerRb.velocity = new Vector3(vInput * speed, playerRb.velocity.y, playerRb.velocity.z);
+        
+        //Grabs the axis for control
         hInput = Input.GetAxis("Horizontal");
         vInput = Input.GetAxis("Vertical");
+
         transform.Translate(Vector3.left * Time.deltaTime * speed *hInput);
 
-        //if player pushes space bar, player go boing boing
-      if(Input.GetKeyDown(KeyCode.Space))
+        //if player pushes space bar, and isOnGround, player go boing boing
+      if(Input.GetKeyDown(KeyCode.Space) && isOnGround == true && extraJumps > 0)
       {
-          playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+          isJumping = true;
+          jumpTimeCounter = jumpTime;
+          playerRb.velocity = Vector3.up * jumpForce;
+          //playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+          isOnGround = false;
+          extraJumps--;
+      } 
+      else if(Input.GetKeyDown(KeyCode.Space) && isOnGround == true && extraJumps == 0)
+      {
+         playerRb.velocity = Vector3.up * jumpForce;
+         isJumping = true;
+         jumpTimeCounter = jumpTime;
       }
+
+      if(Input.GetKey(KeyCode.Space) && isJumping == true)
+      {
+        if(jumpTimeCounter > 0)
+        {
+           //playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+           playerRb.velocity = Vector3.up * jumpForce;
+           jumpTimeCounter -=Time.deltaTime; 
+        }
+        else{
+            isJumping = false;
+        } 
+      }
+
+      if(Input.GetKeyUp(KeyCode.Space))
+      {
+          isJumping = false;
+      }
+
       //if player goes too far to the left, stop movement
       if(transform.position.x > leftBound)
         {
@@ -45,7 +89,15 @@ public class PlayerController : MonoBehaviour
     }
 
     private void OnCollisionEnter(Collision other)
-    {   //if player touches respawn platform, isTakeDamage becomes false
+    {   
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+            //extraJumps = 2;
+            Debug.Log("Grounded");
+        }
+        
+        //if player touches respawn platform, isTakeDamage becomes false
         if(other.gameObject.CompareTag("Respawn"))
         {
             isTakeDamage = false;
