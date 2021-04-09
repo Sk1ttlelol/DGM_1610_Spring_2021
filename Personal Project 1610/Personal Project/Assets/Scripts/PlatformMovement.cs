22ee6247f2d8e55559d4a40845191fb76a77b6d5
Darick Cochrane <boomrocks24@gmail.com>
2021-04-08 22:22:56 -0600

    Enemies move up and down based on top and bottom bound triggers

---
+++ Personal Project 1610/Personal Project/Assets/Scripts/PlatformMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformMovement : MonoBehaviour
+{
+
+    public float speed;
+    private float leftBound;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(gameObject.CompareTag("PLAT_MOVE_LEFT"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        }
+        
+        if(gameObject.CompareTag("PLAT_MOVE_RIGHT"))
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+
+
+
+        //if(transform.position.x > leftBound)
+    }
+}
---
+++ Personal Project 1610/Personal Project/Assets/Scripts/StationaryEnemyMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationaryEnemyMovement : MonoBehaviour
+{
+
+    public float topBound = 24;
+    public float bottomBound = 15;
+    public float speed;
+    public bool topBoundReached = true;
+    public bool bottomBoundReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y > topBound)
+        {
+            topBoundReached = true;
+            bottomBoundReached = false;
+            Debug.Log("top bound hit");
+        }
+
+        if(transform.position.y < bottomBound)
+        {
+            topBoundReached = false;
+            bottomBoundReached = true;
+        }
+
+        if(transform.position.y < topBound && topBoundReached == false)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+        }
+
+        if(transform.position.y > bottomBound && bottomBoundReached == false)
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * speed);
+        }
+    }
+}
