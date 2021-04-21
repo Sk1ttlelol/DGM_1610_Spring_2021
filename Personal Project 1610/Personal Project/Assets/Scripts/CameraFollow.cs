1deea5d99334fe2b94f50412c8c971e6e625ce8c
Darick Cochrane <boomrocks24@gmail.com>
2021-04-21 15:45:30 -0600

    Made Camera Follow better Personal Project

--- Personal Project 1610/Personal Project/Assets/Scripts/CameraFollow.cs
+++ Personal Project 1610/Personal Project/Assets/Scripts/CameraFollow.cs
@@ -4,12 +4,9 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    
-    public float speed;
-    public float hInput;
-    public float leftBound = 11;
-    //variable to grab player controller script
-    private PlayerController playerControllerScript;
+    public Transform target;
+    public float smoothSpeed = 10f; 
+    public Vector3 offset;
 
     public Vector3 cameraSpawnPos = new Vector3(10,17,16);
 
@@ -17,24 +14,13 @@ public class CameraFollow : MonoBehaviour
     void Start()
     {
         transform.position = cameraSpawnPos;
-        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {  
-        hInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed *hInput);
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp (transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+        transform.position = smoothedPosition;
 
-        //if position of camera goes too far left, stop movement
-        if(transform.position.x > leftBound)
-        {
-            transform.position = new Vector3(leftBound, transform.position.y, transform.position.z);
-        }
-        //if isTakeDamage from playerControllerScript becomes true, move camera to respawn postiion to continue following player
-        if(playerControllerScript.isTakeDamage == true)
-        {
-            transform.position = cameraSpawnPos;
-        }
     }
 }
